f88c9220b8c3eb7b21ea204d2fc416d78c3f23d0
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-10-03 20:27:17 +0100

    add mail service

--- SRC/Backend/Meat-Station-BE/Meat-Station.Domain/Models/UserRoles.cs
+++ SRC/Backend/Meat-Station-BE/Meat-Station.Domain/Models/UserRoles.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,9 @@ namespace Meat_Station.Domain.Models
     {
         [Key]
         public int Id { get; set; }
+        [ForeignKey(nameof(User))]
         public int UserId { get; set; }
+        [ForeignKey(nameof(Role))]
         public int RoleId { get; set; }
     }
 }
---
+++ SRC/Backend/Meat-Station-BE/Meat-Station.Service/MailService/Repository/MailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meat_Station.Service.MailService.Repository
+{
+    public class MailService
+    {
+    }
+}
